d486da8138f615aaff22039b7b44c0d648b0574c
Augustin <augustinvathonne@gmail.com>
2025-06-20 16:28:37 +0200

    style(fix): fixing after rebase

--- front/src/components/pages/Dashboard/Dashboard.jsx
+++ front/src/components/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import DashboardUser from "../organisms/DashboardUser/DashboardUser";
-import DashboardProduct from "../organisms/DashboardProduct/DashboardProduct";
-import DashboardWarehouse from "../organisms/DashboardWarehouse/DashboardWarehouse";
+import DashboardUser from "../../organisms/DashboardUser/DashboardUser";
+import DashboardProduct from "../../organisms/DashboardProduct/DashboardProduct";
+import DashboardWarehouse from "../../organisms/DashboardWarehouse/DashboardWarehouse";
 import MainLayout from "../../templates/MainLayout";
+import apiClient from "../../../utils/apiClient";
 
 export default function Dashboard() {
     const [users, setUsers] = useState([]);
